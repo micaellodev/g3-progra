1c5392408caf5156d33e556f4756225c104772a4
jpabloav <jpabloav127@gmail.com>
2025-05-24 01:44:49 -0500

    humilde aporte

--- frontend/src/pages/client/Carrito.jsx
+++ frontend/src/pages/client/Carrito.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import CarritoItems from '../../components/Carrito/CarritoItems';
 import CarritoResumen from '../../components/Carrito/CarritoResumen';
 import styles from '../../styles/Carrito.module.css';
@@ -11,14 +12,15 @@ export const Carrito = () => {
   return (
     <div className={styles.carritoWrapper}>
       <div className={styles.carritoContenido}>
-        <h1>Carrito 2</h1>
+        <h1>Carrito</h1>
         <CarritoItems juegos={juegosEnCarrito} />
+        <Link to="/inicio" className={styles.botonSeguirComprando}>
+          Seguir comprando
+        </Link>
       </div>
       <CarritoResumen juegos={juegosEnCarrito} />
     </div>
-  )
-}
-
-
+  );
+};
 
 export default Carrito;
