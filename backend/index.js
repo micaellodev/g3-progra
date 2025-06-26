39c9b961d681cc6351e4697fd43346536d33f9cc
R3nz07 <138803881+R3nz07@users.noreply.github.com>
2025-06-26 12:29:10 -0500

    avance js

---
+++ backend/index.js
@@ -0,0 +1,80 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors());
+
+function crearProducto(id,nombre, stock, precio, ){
+const obj = {id,nombre,stock,precio}
+obj["Estado"]="Disponible";
+
+return obj;
+}
+
+const listadoProductos = [
+    crearProducto("1", "Producto 1", 10, 100),  
+    crearProducto("2", "Producto 2", 5, 200),
+    crearProducto("3", "Producto 3", 0, 300),
+]
+
+function borrarProducto(id){
+    const productoIndex = productos.findIndex(p => p.id === id);
+    if (productoIndex !== -1) {
+        productos.splice(productoIndex, 1);
+        return true;
+    }
+    return false;
+}
+//obtener un producto específico
+app.get("/productos/:id", (req, res) => {
+    const producto = obtenerProductoPorId(req.params.id);
+    if (producto) {
+        res.json(producto);
+    } else {
+        res.status(404).json({ error: "Producto no encontrado" });
+    }
+});
+
+//crear un nuevo producto
+app.post("/productos", (req, res) => {
+    const { id, nombre, stock, precio } = req.body;
+    const nuevoProducto = crearProducto(id, nombre, stock, precio);
+    productos.push(nuevoProducto);
+    res.status(201).json(nuevoProducto);
+});
+
+//actualizar un producto existente
+app.put('/productos/:id', (req, res) => {
+    const id = req.params.id;
+    const producto = productos.find(p => p.id === id);
+    if (!producto) {
+        return res.status(404).json({ mensaje: "Producto no encontrado" });
+    }
+    const { nombre, stock, precio } = req.body;
+    if (nombre !== undefined) producto.nombre = nombre;
+    if (stock !== undefined) producto.stock = stock;
+    if (precio !== undefined) producto.precio = precio;
+
+    res.json({ mensaje: "Producto actualizado", producto });
+});
+
+app.delete("/productos/:id", (req, res) => {
+    const id = req.params.id;
+    const productoEliminado = borrarProducto(id);
+    if (productoEliminado) {
+        res.status(204).send();
+    } else {
+        res.status(404).json({ error: "Producto no encontrado" });
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Servidor escuchando en http://localhost:${port}`);
+});
+
+
+
+
---
+++ frontend/src/services/tiendaService.js
@@ -0,0 +1,216 @@
+const API_URL = "http://localhost:3000";
+
+// Funciones para productos
+export const CrearProducto = async (id,nombre, stock, precio) => {
+    try {
+        const response = await fetch(`${API_URL}/productos`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({id,nombre,stock,precio}),
+        });
+        if (!response.ok) {
+            throw new Error("Error al crear el producto");
+        }
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+    }
+}
+export const BorrarProducto = async (id) => {
+    try {
+        const response = await fetch(`${API_URL}/productos/${id}`, {
+            method: "DELETE",
+        });
+        if (!response.ok) {
+            throw new Error("Error al borrar el producto");
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const ObtenerProductoPorId = async (id) => {
+    try {
+        const response = await fetch(`${API_URL}/productos/${id}`);
+        if (!response.ok) {
+            throw new Error("Error al obtener el producto");
+        }
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+    }
+}
+export const ActualizarProducto = async (id, producto) => {
+    try {
+        const response = await fetch(`${API_URL}/productos/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(producto),
+        });
+        if (!response.ok) {
+            throw new Error("Error al actualizar el producto");
+        }
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+// Funciones para Usuario
+export const CrearUsuario = async (usuario) => {
+    try {
+        const response = await fetch(`${API_URL}/usuarios`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(usuario),
+        });
+        if (!response.ok) {
+            throw new Error("Error al crear el usuario");
+        }
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const BorrarUsuario = async (id) => {
+    try {
+        const response = await fetch(`${API_URL}/usuarios/${id}`, {
+            method: "DELETE",
+        });
+        if (!response.ok) {
+            throw new Error("Error al borrar el usuario");
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const ObtenerUsuarioPorId = async (id) => {
+    try {
+        const response = await fetch(`${API_URL}/usuarios/${id}`);
+        if (!response.ok) {
+            throw new Error("Error al obtener el usuario");
+        }
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const ActualizarUsuario = async (id, usuario) => {
+    try {
+        const response = await fetch(`${API_URL}/usuarios/${id}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(usuario),
+        });
+        if (!response.ok) {
+            throw new Error("Error al actualizar el usuario");
+        }
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+    }
+}
+//Funciones Login Usuario
+export const LoginUsuario = async (usuario) => {
+    try {
+        const response = await fetch(`${API_URL}/usuarios/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(usuario),
+        });
+        if (!response.ok) {
+            throw new Error("Error al iniciar sesión");
+        }
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+//Funciones para Carrito
+export const ObtenerCarrito = async (usuarioId) => {
+    try {
+        const response = await fetch(`${API_URL}/carrito/${usuarioId}`);
+        if (!response.ok) {
+            throw new Error("Error al obtener el carrito");
+        }
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const AgregarProductoAlCarrito = async (usuarioId, productoId) => {
+    try {
+        const response = await fetch(`${API_URL}/carrito/${usuarioId}/productos`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ productoId }),
+        });
+        if (!response.ok) {
+            throw new Error("Error al agregar el producto al carrito");
+        }
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const EliminarProductoDelCarrito = async (usuarioId, productoId) => {
+    try {
+        const response = await fetch(`${API_URL}/carrito/${usuarioId}/productos/${productoId}`, {
+            method: "DELETE",
+        });
+        if (!response.ok) {
+            throw new Error("Error al eliminar el producto del carrito");
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const ActualizarProductoEnCarrito = async (usuarioId, productoId, cantidad) => {
+    try {
+        const response = await fetch(`${API_URL}/carrito/${usuarioId}/productos/${productoId}`, {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ cantidad }),
+        });
+        if (!response.ok) {
+            throw new Error("Error al actualizar el producto en el carrito");
+        }
+        return await response.json();
+    } catch (error) {
+        console.error(error);
+    }
+}   
+
+export const LimpiarCarrito = async (usuarioId) => {
+    try {
+        const response = await fetch(`${API_URL}/carrito/${usuarioId}`, {
+            method: "DELETE",
+        });
+        if (!response.ok) {
+            throw new Error("Error al limpiar el carrito");
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}   
