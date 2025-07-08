f0adbfa679afcaad66014bdb4d1e3f40365de18e
Joel Cordova <joelleonardoaramis@gmail.com>
2025-07-07 21:02:37 -0500

    XD

--- frontend/src/hooks/ProductContext.jsx
+++ frontend/src/hooks/ProductContext.jsx
@@ -1,5 +1,5 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-import { juegos } from '../constantes/consts';
+import { juegos } from '../constantes/Consts';
 
 const ProductContext = createContext();
 
