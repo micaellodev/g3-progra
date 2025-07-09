742ae0ff52864e53425e1afb27531e58227c2440
R3nz07 <138803881+R3nz07@users.noreply.github.com>
2025-07-09 17:13:32 -0500

    changes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,30 +1,12 @@
 import express from 'express';
 import {
-  getUsers,
-  getUserDetail,
-  toggleUserStatus,
-  createUser,
-  updateUser,
-  deleteUser,
-  loginUser,
-  changePassword,
-  recoverPassword,
-  getUserProfile,
-  verifyEmail
+    getAllUsers,
+    getUserById,
 } from '../controllers/UsuarioController.js';
 
 const router = express.Router();
 
-router.get('/', getUsers);
-router.get('/:id', getUserDetail);
-router.post('/', createUser);
-router.put('/:id', updateUser);
-router.delete('/:id', deleteUser);
-router.post('/login', loginUser);
-router.put('/:id/cambiar-contrasena', changePassword);
-router.post('/recuperar-contrasena', recoverPassword);
-router.get('/:id/perfil', getUserProfile);
-router.patch('/:id/status', toggleUserStatus);
-router.get('/verificar-correo/:correo', verifyEmail);
+router.get('/', getAllUsers);
+router.get('/:id', getUserById);
 
 export default router;
