8b9f56ee1cbff76c6e082dd7800c0241f367d63e
MarceloTZ09 <marceloalejandro0204@gmail.com>
2025-07-09 16:31:19 -0500

    userRoute

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,15 +1,31 @@
 import express from 'express';
 
+const router = express.Router();
+
 import {
   getUsers,
   getUserDetail,
-  toggleUserStatus
+  createUser,
+  updateUser,
+  deleteUser,
+  loginUser,
+  changePassword,
+  recoverPassword,
+  getUserProfile,
+  toggleUserStatus,
+  verifyEmail
 } from '../controllers/UsuarioController.js';
-
-const router = express.Router();
 
 router.get('/', getUsers);
 router.get('/:id', getUserDetail);
+router.post('/', createUser);
+router.put('/:id', updateUser);
+router.delete('/:id', deleteUser);
+router.post('/login', loginUser);
+router.put('/:id/cambiar-contrasena', changePassword);
+router.post('/recuperar-contrasena', recoverPassword);
+router.get('/:id/perfil', getUserProfile);
 router.patch('/:id/status', toggleUserStatus);
+router.get('/verificar-correo/:correo', verifyEmail);
 
 export default router;
